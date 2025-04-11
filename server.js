30354afbf2619d3a4dc07a7f16a71063e504d2a6
root@kali <118610046+burakdevelopment@users.noreply.github.com>
2025-04-12 00:26:21 +0300

    V1
    try #1

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
+
+
+dotenv.config();
+
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+connectDB();
+
+
+app.use('/api/auth', authRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Sunucu ${PORT} portunda çalışıyor`);
+});
